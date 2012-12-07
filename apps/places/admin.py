4c7a153f3e0f229b79f98c83049f00f51b3de956
Gleb grishin <griglmail@gmail.com>
2012-12-07 14:41:58 +0400

    lab в админке

--- apps/places/admin.py
+++ apps/places/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import City, Clinic, Branch, Drugstore
+from models import City, Clinic, Branch, Drugstore, Lab
 
-admin.site.register(City)
-admin.site.register(Clinic)
-admin.site.register(Branch)
-admin.site.register(Drugstore)
+admin.site.register(City,)
+admin.site.register(Clinic,)
+admin.site.register(Branch,)
+admin.site.register(Drugstore,)
+admin.site.register(Lab,)
