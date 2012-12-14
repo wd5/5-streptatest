2895c0ba7fb4187ba0158f20e3acd5834cc1d0f7
Gleb grishin <griglmail@gmail.com>
2012-12-14 15:16:30 +0400

    поправил анимацию и слайдеры на главной

--- media/js/script.js
+++ media/js/script.js
@@ -1,42 +1,60 @@
+var IndexPageSliders = {
+    doctorsCarousel: function(){
+        $('#doctors_carousel ul').bxSlider({
+            pager: false,
+            nextSelector: '#doc_carousel_r',
+            prevSelector: '#doc_carousel_l',
+            nextText: '',
+            prevText: ''
+        });
+    },
+    patientsCarousel: function(){
+        $('#patients_carousel ul').bxSlider({
+            pager: false,
+            nextSelector: '#patient_carousel_r',
+            prevSelector: '#patient_carousel_l',
+            nextText: '',
+            prevText: ''
+        });
+    }
+};
+
 var SwitchSlider = function(){
     $('.index_reviews #patients_link').on('click', function(){
         $('#doctors_carousel').hide();
         $('#doctors_link').parents('li').removeClass('curr');
         $('#patients_carousel').show();
         $('#patients_link').parents('li').addClass('curr');
+        IndexPageSliders.patientsCarousel();
+        return false;
     });
     $('.index_reviews #doctors_link').on('click', function(){
         $('#patients_carousel').hide();
         $('#patients_link').parents('li').removeClass('curr');
         $('#doctors_carousel').show();
         $('#doctors_link').parents('li').addClass('curr');
-    });
-}
-
-var IndexPageSliders = function(){
-    $('#doctors_carousel ul').bxSlider({
-        pager: false,
-        nextSelector: '#doc_carousel_r',
-        prevSelector: '#doc_carousel_l',
-        nextText: '',
-        prevText: ''
-    });
-    $('#patients_carousel ul').bxSlider({
-        pager: false,
-        nextSelector: '#patient_carousel_r',
-        prevSelector: '#patient_carousel_l',
-        nextText: '',
-        prevText: ''
+        IndexPageSliders.doctorsCarousel();
+        return false;
     });
 }
 
 var IndexBoxAnimate = function(){
     var ShowBox = function(){
-        $('.index_box_img_1').css({ 'background': "url('/media/img/box_2.png') no-repeat" }).delay(1000).css({ 'background': "url('/media/img/box_3.png') no-repeat" });
-    }
+        setTimeout( function(){
+            $('.index_box_img_1').css({ 'background': "url('/media/img/box_2.png') no-repeat" });
+            setTimeout( function(){
+                $('.index_box_img_1').css({ 'background': "url('/media/img/box_3.png') no-repeat" });
+            },100);
+        },100);
+    };
     var ShowRoom = function(){
-        $('.index_box_img_1').css({ 'background': "url('/media/img/box_2.png') no-repeat" }).delay(1000).css({ 'background': "url('/media/img/box_1.png') no-repeat" });
-    }
+        setTimeout( function(){
+            $('.index_box_img_1').css({ 'background': "url('/media/img/box_2.png') no-repeat" });
+            setTimeout( function(){
+                $('.index_box_img_1').css({ 'background': "url('/media/img/box_1.png') no-repeat" });
+            },100);
+        },100);
+    };
     $('.index_box_img').hover(ShowBox, ShowRoom);
 }
 
@@ -69,7 +87,7 @@ var DrugstoresMapChange = function(){
 
 $(function(){
     SwitchSlider();
-    IndexPageSliders();
+    IndexPageSliders.doctorsCarousel();
     IndexBoxAnimate();
     ShowFullReview();
     HideFullReview();
